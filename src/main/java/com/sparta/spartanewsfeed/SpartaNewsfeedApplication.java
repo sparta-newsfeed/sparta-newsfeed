e61302baee67abf37b9f03380eae8d858628f8ca
kimwanyoung <dhks2869@gmail.com>
2024-10-18 15:07:12 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/com/sparta/spartanewsfeed/SpartaNewsfeedApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartanewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaNewsfeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaNewsfeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartanewsfeed/SpartaNewsfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartanewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaNewsfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
