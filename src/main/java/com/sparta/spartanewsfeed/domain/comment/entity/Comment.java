43b31f57b570fb065a104e1914a8a7467bfd7df1
wootaepark <pakdal99@gmail.com>
2024-10-21 11:38:57 +0900

    update : ì양방향 매핑에서 단방향 매핑으로 수정

--- src/main/java/com/sparta/spartanewsfeed/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/spartanewsfeed/domain/comment/entity/Comment.java
@@ -1,9 +1,13 @@
 package com.sparta.spartanewsfeed.domain.comment.entity;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.sparta.spartanewsfeed.domain.Timestamp;
 import com.sparta.spartanewsfeed.domain.article.Article;
 import com.sparta.spartanewsfeed.domain.member.Member;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -35,10 +39,12 @@ public class Comment extends Timestamp {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "article_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Article article;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	@JoinColumn(name = "member_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Member author;
 
 	public void update(String body) {
