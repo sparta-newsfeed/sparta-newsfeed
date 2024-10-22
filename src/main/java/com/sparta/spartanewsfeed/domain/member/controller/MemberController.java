5a6d1e09e623cf63de834fd11f83265adf14702b
hemgom <reborn.april23@gmail.com>
2024-10-22 11:57:16 +0900

    feat: 비밀번경요청 DTO 수정

--- src/main/java/com/sparta/spartanewsfeed/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/spartanewsfeed/domain/member/controller/MemberController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class MemberController {
 	private final MemberService memberService;
 
-	public MemberController(final MemberService memberService) {
+	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
 
