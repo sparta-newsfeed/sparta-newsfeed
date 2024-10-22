136e78849f108563a99a2144d4fc6734f2ac462c
kimwanyoung <dhks2869@gmail.com>
2024-10-22 17:05:11 +0900

    update : repsonse dto 날짜 및 작성자 추가

--- src/main/java/com/sparta/spartanewsfeed/domain/article/controller/dto/ArticleResponseDto.java
+++ src/main/java/com/sparta/spartanewsfeed/domain/article/controller/dto/ArticleResponseDto.java
@@ -1,6 +1,9 @@
 package com.sparta.spartanewsfeed.domain.article.controller.dto;
 
+import java.time.LocalDateTime;
+
 import com.sparta.spartanewsfeed.domain.article.entity.Article;
+import com.sparta.spartanewsfeed.domain.member.dto.ResponseMember;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,22 +19,30 @@ public class ArticleResponseDto {
 	private Long id;
 	private String title;
 	private String body;
+	private ResponseMember author;
 	private boolean isLiked;
+	private LocalDateTime updatedAt;
 
 	public static ArticleResponseDto from(Article article) {
+		ResponseMember responseAuthor = ResponseMember.make(article.getAuthor());
 		return ArticleResponseDto.builder()
 			.id(article.getId())
 			.title(article.getTitle())
+			.author(responseAuthor)
+			.updatedAt(article.getUpdatedAt())
 			.body(article.getBody())
 			.build();
 	}
 
 	public static ArticleResponseDto of(Article article, boolean isLiked) {
+		ResponseMember responseAuthor = ResponseMember.make(article.getAuthor());
 		return ArticleResponseDto.builder()
 			.id(article.getId())
 			.title(article.getTitle())
+			.author(responseAuthor)
 			.isLiked(isLiked)
 			.body(article.getBody())
+			.updatedAt(article.getUpdatedAt())
 			.build();
 	}
 }
