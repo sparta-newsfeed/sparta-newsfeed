57a7cb33dfafc98abe0b45066cff3e02e868550c
hemgom <reborn.april23@gmail.com>
2024-10-22 16:57:57 +0900

    feat: 회원비밀번호 수정시 사용되는 정규표현식 수정

--- src/main/java/com/sparta/spartanewsfeed/domain/member/dto/UpdatePassword.java
+++ src/main/java/com/sparta/spartanewsfeed/domain/member/dto/UpdatePassword.java
@@ -2,14 +2,12 @@ package com.sparta.spartanewsfeed.domain.member.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdatePassword {
     @NotNull
-    @Size(min = 6)
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+])[A-Za-z\\d!@#$%^&*()_+]{8,}$")
     private String newPassword;
 
     @NotNull
