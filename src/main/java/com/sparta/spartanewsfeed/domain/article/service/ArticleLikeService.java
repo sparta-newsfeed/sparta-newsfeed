5e2ec5cbe2e02b044b03bd13289fe3503be7b8ea
hemgom <reborn.april23@gmail.com>
2024-10-23 12:09:53 +0900

    feat: ArticleLikeService 예외처리 적용

--- src/main/java/com/sparta/spartanewsfeed/domain/article/service/ArticleLikeService.java
+++ src/main/java/com/sparta/spartanewsfeed/domain/article/service/ArticleLikeService.java
@@ -1,14 +1,15 @@
 package com.sparta.spartanewsfeed.domain.article.service;
 
-import org.springframework.stereotype.Service;
-
 import com.sparta.spartanewsfeed.domain.article.entity.Article;
 import com.sparta.spartanewsfeed.domain.article.entity.ArticleLike;
 import com.sparta.spartanewsfeed.domain.article.repository.ArticleLikeRepository;
 import com.sparta.spartanewsfeed.domain.article.repository.ArticleRepository;
 import com.sparta.spartanewsfeed.domain.member.Member;
-
+import com.sparta.spartanewsfeed.exception.customException.NotFoundEntityException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.sparta.spartanewsfeed.exception.enums.ExceptionCode.NOT_FOUND_ARTICLE;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +20,8 @@ public class ArticleLikeService {
 
 	public void toggleLike(Long articleId, Member member) {
 		Article article = articleRepository.findById(articleId)
-			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물 입니다."));
+			.orElseThrow(() -> new NotFoundEntityException(NOT_FOUND_ARTICLE));
+//			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물 입니다."));
 
 		articleLikeRepository.findArticleLikeByArticleAndMember(article, member)
 			.ifPresentOrElse(
